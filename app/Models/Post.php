0c4759d3aca11ee95e6e8829cfddd53cce3e1bf1
NandaBowo <nandabowo.nb@gmail.com>
2021-09-01 22:19:44 +0700

    Create Post Form

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
 
     protected $guarded = ['id'];
     protected $with = ['category', 'author'];
@@ -48,5 +49,14 @@ class Post extends Model
     public function getRouteKeyName()
     {
         return 'slug';
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
